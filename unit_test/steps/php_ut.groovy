ede31f4cb49e91b67c1b528e667961a65217fab2
m.ehab@suiiz.com <m.ehab@suiiz.com>
2023-10-25 17:04:32 +0300

    test

--- unit_test/steps/php_ut.groovy
+++ unit_test/steps/php_ut.groovy
@@ -1,21 +1,18 @@
-void call(){
-   
+void call() {
     def installdependencies() {
-        println "install dependencies... "
-          sh (
-            returnStdout: true,
-            script: """
-            apt update && apt upgrade -y && apt install composer -y && composer install
-            """
-    )
+        echo "install dependencies... "
+        sh """
+            apt update
+            apt upgrade -y
+            apt install composer -y
+            composer install
+        """
     }
-   
+
     def runUnitTestsAndSetGitHubStatus() {
-        println "run unit test"
+        echo "run unit test"
         catchError(buildResult: 'FAILURE', stageResult: 'FAILURE') {
             sh 'php artisan test'
         }
     }
-
-
 }
