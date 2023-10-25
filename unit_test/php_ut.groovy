3ed57e7e917138ec23cd2878f08605b0bf51bf34
m.ehab@suiiz.com <m.ehab@suiiz.com>
2023-10-25 14:49:14 +0300

    updat unit test

--- unit_test/php_ut.groovy
+++ unit_test/php_ut.groovy
@@ -1,27 +1,28 @@
 void call(){
-    def installdependencies() {
-    println "install dependencies... "
-    installpack = sh (
-        returnStdout: true,
-        script: """
-        apt update && apt upgrade -y && apt install composer -y && composer install
-        """
-    )
-    }
+    println "hello from unit test"
+    // def installdependencies() {
+    // println "install dependencies... "
+    // installpack = sh (
+    //     returnStdout: true,
+    //     script: """
+    //     apt update && apt upgrade -y && apt install composer -y && composer install
+    //     """
+    // )
+    // }
 
-    def runUnitTestsAndSetGitHubStatus() {
-        catchError(buildResult: 'FAILURE', stageResult: 'FAILURE') {
-            sh 'php artisan test'
-        }
-        script {
-            def currentBuildResult = currentBuild.resultIsBetterOrEqualTo('SUCCESS') ? 'SUCCESS' : 'FAILURE'
-            updateGitHubStatus(currentBuildResult)
-        }
-    }
+    // def runUnitTestsAndSetGitHubStatus() {
+    //     catchError(buildResult: 'FAILURE', stageResult: 'FAILURE') {
+    //         sh 'php artisan test'
+    //     }
+    //     script {
+    //         def currentBuildResult = currentBuild.resultIsBetterOrEqualTo('SUCCESS') ? 'SUCCESS' : 'FAILURE'
+    //         updateGitHubStatus(currentBuildResult)
+    //     }
+    // }
 
-    def updateGitHubStatus(buildStatus) {
-        def github = getGitHub()
-        def pr = github.getPullRequest(env.CHANGE_ID)
-        pr.createReview('Automated Jenkins Build', buildStatus, 'Automated Jenkins build result')
-    }
+    // def updateGitHubStatus(buildStatus) {
+    //     def github = getGitHub()
+    //     def pr = github.getPullRequest(env.CHANGE_ID)
+    //     pr.createReview('Automated Jenkins Build', buildStatus, 'Automated Jenkins build result')
+    // }
 }
