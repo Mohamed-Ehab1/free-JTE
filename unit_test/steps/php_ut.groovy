74f1cb3633116d2bf42a2efeaca8f3ef9d365f45
m.ehab@suiiz.com <m.ehab@suiiz.com>
2023-11-04 15:08:09 +0200

    update php-unit test

--- unit_test/steps/php_ut.groovy
+++ unit_test/steps/php_ut.groovy
@@ -8,12 +8,6 @@ void call() {
 def installdependencies() {
         echo "install dependencies... "
         sh """
-            apt update
-            apt install lsb-release ca-certificates apt-transport-https software-properties-common -y
-            add-apt-repository --remove ppa:ondrej/php
-            add-apt-repository ppa:ondrej/php      
-            apt update
-            apt install php -y php8.0 php8.0-common
             apt install composer -y
             composer install
         """
