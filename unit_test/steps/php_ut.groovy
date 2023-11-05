e9755633c2955bc408c3db2e4f386ab06f427028
m.ehab@suiiz.com <m.ehab@suiiz.com>
2023-11-05 10:47:34 +0200

    upate JTE

--- unit_test/steps/php_ut.groovy
+++ unit_test/steps/php_ut.groovy
@@ -9,6 +9,8 @@ def installdependencies() {
         echo "install dependencies... "
         sh """
             composer install
+            export APP_KEY=base64:Eipawu6NuSTDXkeqMKlRarcgptPOn0VlAq264pobd9g=
+
         """
     }
 
