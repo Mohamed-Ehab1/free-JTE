cfbce42910d6927ade15e7fda76b6f30e65fa188
m.ehab@suiiz.com <m.ehab@suiiz.com>
2023-11-20 18:07:30 +0200

    lp

--- first_library/steps/helloworld.groovy
+++ first_library/steps/helloworld.groovy
@@ -15,7 +15,7 @@ def inject_ssh(){
                 //writeFile file: 'ssh.txt', text: SSH_CONTENT
                 
                 sh """
-                echo $SSH_CONTENT >> ssh.txt
+                echo $SSH_KEY >> ssh.txt
                 echo "" >> ssh.txt
                 """
             }    
