51a2320d8617d53f8527cf5a8f90db95854bc0e2
m.ehab@suiiz.com <m.ehab@suiiz.com>
2023-11-20 16:17:41 +0200

    kosm commit

--- first_library/steps/helloworld.groovy
+++ first_library/steps/helloworld.groovy
@@ -1,42 +1,46 @@
 void call() {
     println "hello world"
-    
+    inject_ssh()
 }
 
 
-dir ("/home/jenkins") {
+
 
 def inject_ssh(){
-    println "inject authentication for composer to build image"
-    withCredentials([sshUserPrivateKey(credentialsId: 'Bitbucket', keyFileVariable: 'SSH_KEY')]) {
-        script {
-            // Write the SSH key to the file
-            writeFile file: 'ssh.txt', text: SSH_KEY.replaceAll("\\\\n", "\n")
-            sh 'echo "" >> ssh.txt'
-        }    
+    dir ("/home/jenkins") {
+        println "inject authentication for composer to build image"
+        withCredentials([sshUserPrivateKey(credentialsId: 'Bitbucket', keyFileVariable: 'SSH_KEY')]) {
+            script {
+                // Write the SSH key to the file
+                writeFile file: 'ssh.txt', text: SSH_KEY.replaceAll("\\\\n", "\n")
+                sh 'echo "" >> ssh.txt'
+            }    
+        }
     }
-    
 }
 
 def install_open_shh() {
-    script {
-        bash """
-        apt update
-        apt install openssh -y
-        """
+    dir ("/home/jenkins") {
+        script {
+            bash """
+            apt update
+            apt install openssh -y
+            """
+        }
     }
 }
 
 def authenticate_bitbucket(){
-   script{ sh """
-    mkdir -p ~/.ssh
-    mv ssh.txt ~/.ssh/id_ed25519
-    chmod 400 ~/.ssh/id_ed25519
-    ssh -o StrictHostKeyChecking=no -T git@bitbucket.org
-    """
-   }
+    dir ("/home/jenkins") {    
+        script{ sh """
+         mkdir -p ~/.ssh
+         mv ssh.txt ~/.ssh/id_ed25519
+         chmod 400 ~/.ssh/id_ed25519
+         ssh -o StrictHostKeyChecking=no -T git@bitbucket.org
+         """
+        }
+    }
 
 }
 
 
-}
