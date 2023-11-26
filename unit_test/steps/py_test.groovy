61d15a5e166ce03a14dd26eb0045c14094db7d04
m.ehab@suiiz.com <m.ehab@suiiz.com>
2023-11-26 14:33:14 +0200

    added try catch

--- unit_test/steps/py_test.groovy
+++ unit_test/steps/py_test.groovy
@@ -2,13 +2,14 @@ void call () {
    try { 
     install_requirements()                
     }
-catch (Exception e) {
-        echo "An exception occurred: ${e.getMessage()}"
-
-        }
-finally {
-        echo "Test is finished completely."
-        }
+    
+    catch (Exception e) {
+            echo "An exception occurred: ${e.getMessage()}"
+    }
+    
+    finally {
+            echo "Test is finished completely."
+    }
 }
 
 def install_requirements() {
